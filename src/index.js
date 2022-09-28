b64e03a70cd636871320c38ed8d6d8d27b160a1f
user <yashiroisanagi@gmail.com>
2022-09-28 10:37:12 -0500

    ♻️ Swapped BrowserRouter to HashRouter

--- src/index.js
+++ src/index.js
@@ -1,21 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { HashRouter as Router } from "react-router-dom";
 
-import { Provider } from 'react-redux';
-import { store } from './store/store';
-import App from './App';
-import InlineStyleComponent from './script';
+import { Provider } from "react-redux";
+import { store } from "./store/store";
+import App from "./App";
+import InlineStyleComponent from "./script";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   //<React.StrictMode>
   <>
     <InlineStyleComponent />
     <Provider store={store}>
-      <BrowserRouter>
+      <Router basename='/'>
         <App />
-      </BrowserRouter>
+      </Router>
     </Provider>
   </>
   //</React.StrictMode>
